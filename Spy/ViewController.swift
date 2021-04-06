6f55d01c1ce1300ed757ec175cf81d2481982545
locust-dev <rrrr.ty30@gmail.com>
2021-04-06 16:09:33 +0300

    Initial Commit

---
+++ Spy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spy
+//
+//  Created by Илья Тюрин on 06.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
